e934509d4ac2a5b66aa24a1177298be30d7a4d9e
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2024-11-03 22:45:58 +0700

    add readme.md

---
+++ readme.md
@@ -0,0 +1,12 @@
+### API Documentation
+
+The API documentation is available at the following endpoints:
+
+- Swagger UI: `localhost:3000/api-docs`
+- OpenAPI Document in JSON: `localhost:3000/api-docs/json`
+
+### Superadmin Credentials
+
+- username : `superaadmin`
+- Password: `superadminpassword`
+- Email: `superadmin@gmail.com`
--- routes/documentation.js
+++ routes/documentation.js
@@ -4,6 +4,10 @@ const swaggerDocumentation = require('../docs/swagger.json')
 
 router.use("/", swaggerUI.serve);
 router.use("/", swaggerUI.setup(swaggerDocumentation));
-
+router.get("/json", (req, res) => {
+    console.log("Swagger Documentation:", swaggerDocumentation);
+    res.set('Cache-Control', 'no-store');
+    res.json(swaggerDocumentation);
+  });
 
 module.exports = router;
