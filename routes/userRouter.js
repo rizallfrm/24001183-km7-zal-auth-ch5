9bffae9c7369526a9ee8fed99119318798be7137
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2024-11-03 18:04:44 +0700

    update userRouter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,5 +3,9 @@ const router = require("express").Router();
 const { userController } = require("../controllers");
 
 router.get("", userController.getAllUsers);
+router.get("/:id", userController.getUserById);
+router.put("/:id", userController.updateUser);
+router.post("", userController.createUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
